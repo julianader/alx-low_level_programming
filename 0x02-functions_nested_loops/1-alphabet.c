62a12a942028bd0671c2c1ee7e889d64d301df32
julianader <julianader4@gmail.com>
2023-03-21 18:11:10 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while(ch <= 'z')
+	{
+		_putchar(ch);
+		++ch;
+	}
+
+	_putchar('\n');
+}
