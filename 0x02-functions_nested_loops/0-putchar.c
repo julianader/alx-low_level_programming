1f37347f44d237bd23ce998f57e8fd48647957c5
julianader <julianader4@gmail.com>
2023-03-21 17:48:16 -0700

    tsk 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+include "main.h"
+
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char arr[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; ++i)
+		_putchar(arr[i]);
+	_putchar('\n');
+
+	return (0);
+}
