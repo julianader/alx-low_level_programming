647c5a2cc0e8b9bb1bc0d007e980535daced2dc8
Julia Nader <julianader4@gmail.com>
2023-03-24 14:45:28 -0700

    task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,17 +9,21 @@
 
 void print_diagonal(int n)
 {
-	int postn, space;
-
+	int x, y;
+	
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-		for (postn = 1; postn <= n; ++postn)
+		for (x = 0; x < n; x++)
 		{
-			for (space = 1; space <= postn; ++space)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
+			for (y = 0; y < x; y++)
+			{
+				_putchar(32);
+			}	
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
