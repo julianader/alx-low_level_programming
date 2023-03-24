048aa2e2a4392f5acc6b12df5d52d9d5c795a98b
Julia Nader <julianader4@gmail.com>
2023-03-24 14:30:32 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,19 +9,20 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int num, row, count;
 
-	for (i = 0; i < 11; i++)
+	for (row = 1; row <= 10; ++row)
 	{
-		for (j = 0; j <= 14; ++j)
+		for (count = 0; count <= 14; ++count)
 		{
-			i = j;
-			if (j > 9)
+			num = count;
+			if (count > 9)
 			{
 				_putchar(1 + 48);
-				i = j % 10;
+				num = count % 10;
 			}
-			_putchar('\n');
+			_putchar(num + 48);
 		}
+		_putchar('\n');
 	}
 }
