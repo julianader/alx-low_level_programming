d398c0d8208c5c873d3d00bf35af94b5c7d3319c
Julia Nader <julianader4@gmail.com>
2023-03-24 13:49:34 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if input is digit between 0 - 9
+ *
+ * @c: input
+ *
+ * Return: 1 if is digit, 0 if not
+*/
+
+int mul(int x, int y)
+{
+	return x*y;
+}
