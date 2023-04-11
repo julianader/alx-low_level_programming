d1760d01beed7326f8debb2659829072ea078874
Julia Nader <julianader4@gmail.com>
2023-04-11 12:20:32 -0700

    betty edits

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,21 +10,21 @@
  * Return: array @a
 */
 
-char * create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
-    char * arr; 
-    
-    arr = malloc(sizeof(char) * size);
-    
-    if (size == 0)
+	unsigned int i;
+       	char *arr; 
+	
+	arr = malloc(sizeof(char) * size);
+	
+	if (size == 0)
 		return (NULL);
-		
+
 	if (arr == NULL)
 		return (NULL);
-		
-    for (i = 0; i < size; i++)
-        arr[i] = c;
-        
-    return (arr);
+
+	for (i = 0; i < size; i++)
+        	arr[i] = c;
+
+	return (arr);
 }
